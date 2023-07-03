6f71c3a6eb4c186d7e715c15d4a5299e9e6d9cfa
peterjaap <peterjaap@elgentos.nl>
2023-07-03 16:35:38 +0200

    Added Mermaid JS, close #3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import Alpine from 'alpinejs';
 import Focus from '@alpinejs/focus';
+import mermaid from 'mermaid';
 
 import './clipboard';
 import './components/search';
@@ -13,6 +14,11 @@ document.addEventListener('DOMContentLoaded', () => {
     if (document.querySelector('#docsScreen')) {
         import('./docs.js');
     }
+
+    mermaid.initialize({
+        startOnLoad: true,
+        theme: 'default'
+    });
 
     import('./components/accessibility');
 });
